dc2e8fea188962f5128ebc321b90863ca4e44ffd
deekshith024 <105255904+deekshith024@users.noreply.github.com>
2023-03-13 16:42:49 -0400

    display-product-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ProductTable from './ProductTable';
+import AddProductForm from './AddProductForm';
 
-function App() {
+const App = () => {
+  const products = [
+    {
+      productId: 1,
+      productName: 'Product 1',
+      productCategory: 'Category 1',
+      supplier: 'Supplier 1',
+      price: 10.99
+    },
+    {
+      productId: 2,
+      productName: 'Product 2',
+      productCategory: 'Category 2',
+      supplier: 'Supplier 2',
+      price: 20.99
+    },
+    // add more products here...
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Product List</h1>
+      <ProductTable products={products} />
+      <AddProductForm />
     </div>
   );
-}
+};
 
 export default App;
+
+
